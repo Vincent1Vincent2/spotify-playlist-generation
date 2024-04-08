280026379aa91ec122d831fb72789174d2011ed4
Vincent <Vincent.ahlin99@gmail.com>
2024-04-08 15:53:00 +0200

    General structure and routing

---
+++ src/Layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <main>
+      <header></header>
+      <Outlet />
+    </main>
+  );
+};
+
+export default Layout;
