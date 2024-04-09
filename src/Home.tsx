1b08f3d21dd843ced8b61decb8b7ad70da72b417
Vincent <Vincent.ahlin99@gmail.com>
2024-04-09 11:16:31 +0200

    Styling done!

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,9 +3,9 @@ import "./index.css";
 
 export default function Home() {
   return (
-    <main className="heroSec">
-      <div className="hero">
-        <h1>Welcome!</h1>
+    <main className="heroSection">
+      <div className="landingPage">
+        <h1 className="welcomeText">Welcome!</h1>
         <p className="description">It's time to get a personalized playlist.</p>
         <Link className="playlistLink" to="/playlist">
           Generate Playlist
